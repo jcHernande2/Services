88c221fb4d0fe7046d6cbde0aabaec786bf1e155
jcHernande2 <hhjuan27@gmail.com>
2021-05-11 17:46:06 -0500

    implementacion azure

--- FilesServices/Azure.cs
+++ FilesServices/Azure.cs
@@ -1,11 +1,68 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace Services.FilesServices
 {
     public class Azure
     {
+        private readonly CloudBlobClient _blobClient;
+        private static Azure _instance;
+        private Azure(string connection)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connection);
+             _blobClient = storageAccount.CreateCloudBlobClient();
+        }
+        public static Azure GetInstance(string connection)
+        {
+              if(_instance==null){                 
+                 _instance= new Azure(connection);                	
+              }
+              return _instance;
+        }
+        public bool UploadBlobFromStream(string containerName, string fileName, MemoryStream stream)
+        {
+            if (string.IsNullOrEmpty(containerName) && string.IsNullOrEmpty(fileName))
+            {
+                return false;
+            }
+            try
+            {
+                CloudBlockBlob blockBlob = GetBlockBlob(containerName, fileName);
+                blockBlob.UploadFromStreamAsync(stream);
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }           
+        }             
+        public string GetBlobBase64(string containerName, string fileName)
+        {
+            if (string.IsNullOrEmpty(containerName) && string.IsNullOrEmpty(fileName))
+            {
+                return string.Empty;
+            }
+            try
+            {
+                CloudBlockBlob blockBlob = GetBlockBlob(containerName, fileName);
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    blockBlob.DownloadToStreamAsync(stream);
+                    byte[] buff = stream.ToArray();
+                    return Convert.ToBase64String(buff);                   
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }            
+        }
+        private CloudBlockBlob GetBlockBlob(string containerName, string fileName)
+        {
+            CloudBlobContainer container = _blobClient.GetContainerReference(containerName);
+            return container.GetBlockBlobReference(fileName);
+        }
     }
 }
